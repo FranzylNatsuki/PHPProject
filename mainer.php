24f3550706c39bae9451df9b79d563ee53dddc83
Franz Macalua <franzylbjornlmacalua@su.edu.ph>
2024-11-22 10:29:55 +0800

    Add files via upload

--- mainer.php
+++ mainer.php
@@ -1,4 +1,4 @@
-<?php // whatever
+<?php /// For Scripting
 ?>
 
 <!DOCTYPE html>
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title> CCS1 Project</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -16,22 +16,23 @@
                 <h1 style="margin-bottom: 1px;"> Welcome! </h1>
                 <h3 style="margin-bottom: 4em;"> Please Enter Your Details! </h3>
                 <div class="form-container">
-                    <form>
+                    <form method="post" action="home.php hometown.php gallery.php contact.php">
                     <div class="form-group">
                         <input type="text" id="username" name="username" required placeholder="Username">
                     </div>
                     <div class="form-group" style="position: relative;">
                         <input type="password" id="password" name="password" required placeholder="Password">
-                        <span class="eye-icon" onclick="togglePassword()" style="cursor: pointer;">
-                            &#128065; <!-- Eye icon -->
+                        <span class="eye-icon" id="toggle" onclick="togglePassword()" style="cursor: pointer;">
+                            &#128065;
                         </span>
                     </div>
-                    <button type="submit" class="btn" style="margin-top: 20px">Login</button>
+                    <button type="submit" class="btn">Login</button>
                     </form>
                 </div>
             </center>
         </div>
         <img class="corporateImage" src="https://raw.githubusercontent.com/FranzylNatsuki/PHPProject/refs/heads/main/7871282.png" style="height: 45em;">
     </div>
+    <script src="script.js"></script>
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,13 @@
+function togglePassword() {
+        let passwordField = document.getElementById('password');
+        let eyeIcon = document.getElementById('toggle');
+
+        if (passwordField.type === 'password') {
+            passwordField.type = 'text';
+            eyeIcon.innerHTML = '&#128064;';
+
+        } else {
+            passwordField.type = 'password';
+            eyeIcon.innerHTML = '&#128065;';
+        }
+    }
